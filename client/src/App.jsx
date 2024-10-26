e9d8de3a10614c7b9bd1db99d146655dab27ce7b
kestsaurav21 <kestsaurav21@outlook.com>
2024-10-27 00:01:13 +0530

    BgSlider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import NavBar from './components/NavBar';
 function App() {
 
   return (
-    <div >
+    <div className='bg-[#f7fbff]'>
         <NavBar />
         <Routes>
             <Route path='/' element={<Home />} />
