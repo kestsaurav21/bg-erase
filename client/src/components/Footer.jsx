ea95e8c911bbde0546f6e1e064a405e31b0231e8
kestsaurav21 <kestsaurav21@outlook.com>
2024-10-27 00:40:04 +0530

    Home Page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,13 +3,19 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='w-full h-20 bg-black flex justify-around'>
-        <div className='flex items-center gap-8'>
+    <div className='w-full p-10 bg-black flex justify-around py-18 '>
+        <div className='flex items-center gap-8 justify-around'>
             <img src={assets.logo_icon} alt="" />
 
             <p className='text-white text-xl font-bold'>
             All right reserved. Copyright @bg lift
             </p>
+            <div className='flex'>
+                <img className='cursor-pointer w-16' src={assets.facebook_icon} alt="" />
+                <img className='cursor-pointer w-16' src={assets.twitter_icon} alt="" />
+                <img className='cursor-pointer w-16' src={assets.google_plus_icon} alt="" />
+            </div>
+
         </div>
     </div>
   )
