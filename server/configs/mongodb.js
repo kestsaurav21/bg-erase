fc27a8d8cd1f812baff855004fbf262d1e44da0a
kestsaurav21 <kestsaurav21@outlook.com>
2024-10-28 01:57:52 +0530

    Setup Database Connection

---
+++ server/configs/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected', () => {
+        console.log("DATABASE CONNECTED");
+        
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-erase`)
+}
+
+export default connectDB;
