7ac930d44a0699387208f7a639a6d61654ba9dcd
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-02 23:56:59 +0530

    update: removeBgImage()

--- server/controllers/ImageController.js
+++ server/controllers/ImageController.js
@@ -25,6 +25,29 @@ const removeBgImage = async (req,res) => {
 
             const imagePath = req.file.path;
 
+            //Read image file
+
+            const imageFile = fs.createReadStream(imagePath)
+
+            const formData = new FormData()
+
+            formData.append('image_file', imageFile)
+
+            const { data } =  await axios.post('https://clipdrop-api.co/remove-background/v1', formData, {
+                headers: {
+                    'x-api-key': process.env.CLIPDROP_API_KEY,
+                },
+                responseType: 'arraybuffer'
+            } )
+
+            const base64Image = Buffer.from(data, 'binary').toString('base64');
+
+            const resultImage = `data:${req.file.mimeType}; base64, ${base64Image}`;
+
+            await userModel.findByIdAndUpdate(user._id, {creditBalance: user.createReadStream-1})
+
+            res.json({success:true, resultImage, creditBalance: user.creditBalance-1, message:'Background is removed'});
+
 
             
         } catch (error) {
